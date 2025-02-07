34e0e9857e704e0ab5cacb3feb578b2c4cec2165
GilbertoMonroy67 <g.m.ejecutivos@gmail.com>
2025-02-07 08:41:12 -0600

    ToDoJavaScript

---
+++ manipulando_eventos_en_el_DOM/clase4_adding&removingListener/app.js
@@ -0,0 +1,24 @@
+const container = document.querySelector(".container");
+
+const button = document.querySelector("button");
+
+container.addEventListener("mouseover", ()=>{
+    container.style.backgroundColor = "blue";
+});
+
+container.addEventListener("mouseout", ()=>{
+    container.style.backgroundColor = "red";
+});
+
+// button.addEventListener("click", ()=>{
+//     alert("Button Clicked!");
+// });
+const buttonClickCallBack = () => {
+    alert("Button Clicked!");
+};
+
+button.addEventListener("click", buttonClickCallBack);
+
+setTimeout(() =>{
+    button.removeEventListener("click", buttonClickCallBack)
+}, 2000);
